66b7c91db13b78e432bca829401ede61a61fbcfc
louiaR <louitiani@gmail.com>
2017-12-14 21:03:49 +0000

    Part 1: from exercise 1 to 5

--- election.js
+++ election.js
@@ -5,24 +5,51 @@
 /**
  * 1 - Convert candidates object to array
  */
-function candidatesObjToArray(candidates) {
 
+function candidatesObjToArray(candidates) {
+    var arr = [];
+    for(var i = 1; i < 5; i++){
+        arr.push(candidates[i]);
+    }
+    return arr;
 }
+
 
 /**
  * 2 - Remove any voters who have voted for more than 2 people, or have voted for the same person twice.
 */
-function filterInvalidVoters(voters) {
 
+function filterInvalidVoters(voters) {
+   return voters.filter(function(voter){
+       for(let i = 0; i < voter.votingCard.length; i++){
+        if(voter.votingCard.length > 2 || voter.votingCard[i] === voter.votingCard[i + 1]){
+            return false
+        }else{
+            return true
+        }    
+    }
+   });
 }
+
 
 /**
  * 3 - Add up all the votes cast by the voting population. Note that for two adjacent votes in the vote array,
  * the right vote counts for half of the left vote.
  */
-function runElection(voters, candidates) {
 
+function runElection(validVoters, candidates) {
+    for (let i = 0; i < validVoters.length; i++) {
+        var voterCast = validVoters[i].votingCard
+        for (let j = 0; j < voterCast.length; j++)
+            if (voterCast[j] === voterCast[0]) {
+                candidates[voterCast[j]].numVotes += 1;
+            } else {
+                candidates[voterCast[j]].numVotes += 0.5;
+            }
+    }
+    return candidates;
 }
+
 
 /**
  * 4 - After an election has been run, return the winner
@@ -30,7 +57,15 @@ function runElection(voters, candidates) {
  * Desired return value: {name: "Tamara Faiza", age: 46, party: "Pizza Party", numVotes: 3}
  */
 function getWinner(candidates) {
-
+    var hasMaxVote = 0;
+    var theWinner ;
+    for(var i = 1; i < 5; i++){
+        if(candidates[i].numVotes > hasMaxVote){
+            hasMaxVote = candidates[i].numVotes;
+            theWinner= candidates[i];
+        }
+    }
+    return theWinner;
 }
 
 /**
@@ -38,7 +73,7 @@ function getWinner(candidates) {
  * he/she received
  */
 function winnerMessage(winner) {
-
+    return winner.name + ' has won the election with ' + winner.numVotes + ' votes!';
 }
 
 // A sample population of a small number of voters, stored as an array
