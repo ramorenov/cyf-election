8d503a3a740e5da7b8920e710eee517811d22b8b
gdgfjfjnvvaa <gdgdhd@gmail.com>
2018-01-06 14:47:34 +0000

    added voting population array

--- election-part2.js
+++ election-part2.js
@@ -53,7 +53,13 @@ class Election{
 
 
 // Include your votingPopulation array here.
-let votingPopulation = [];
+let votingPopulation = [
+    new Voter('Jane Finnegan', 19, [1,3]), 
+    new Voter('Norman Beracha', 35, [3,4]), 
+    new Voter('Salome Kadek', 22, [2, 1, 3]), 
+    new Voter('Wei Li', 19, [1,2]), 
+    new Voter('Sam MacKinnon', 59, [1,4])
+];
 
 
 // Include your candidates object here.
