4cb23c9d179929065422fb77b3adc2f365e5d119
abbas mokhtari <mona.azami@gmail.com>
2018-01-14 17:57:16 +0000

    new one

--- election-part2.js
+++ election-part2.js
@@ -7,16 +7,50 @@ const {
     winnerMessage,
 } = require('./election');
 
-//document.getElementById('fetchMe').addEventListener('click',fetchData);
-
-
 const fetch = require('node-fetch');
 let candidates = {
-    // 1 :new Candidate ('Tamara Faiza',46, [1,1], 'Pizza Party'),
-    // 2 :new Candidate ('Aylin Duke', 39,[2,2],  'Foam Party'),
-    // 3 :new Candidate('Clay Roderick',54, [3,4],  'Flat Earth Party'),
-    // 4 :new Candidate( 'Nour al-Din',32, [4,1], 'Pizza Party')
+    1 :new Candidate ('Tamara Faiza',46, [1,1], 'Pizza Party'),
+    2 :new Candidate ('Aylin Duke', 39,[2,2],  'Foam Party'),
+    3 :new Candidate('Clay Roderick',54, [3,4],  'Flat Earth Party'),
+    4 :new Candidate( 'Nour al-Din',32, [4,1], 'Pizza Party')
 };
+class Voter {
+    constructor(name,age,votingCard){
+        this.name = name;
+        this.age = age;
+        this.votingCard = votingCard
+    }
+}
+class Candidate extends Voter {
+	constructor(name, age, votingCard, party) {
+		super(name,age,votingCard);
+        this.party = party;
+        this.numVotes= 0;
+	}
+};
+class Election {
+    constructor(validVoters,candidates) {
+        this.validVoters = validVoters;
+        this.candidates = candidates;
+        this.winner = " ";
+    }
+
+    runElection() {
+        console.log(this.candidates);
+        this.candidates = runElection(this.validVoters, this.candidates);
+          this.getWinner();
+          
+    }
+    getWinner() {
+        this.winner = getWinner(this.candidates);
+    };
+
+    printWinnerMessage() {
+        return  winnerMessage(this.winner);
+    }
+}
+
+
 let votingPopulation = [
     new Voter('Jane Finnegan', 19, [1,3]),
     new Voter('Norman Beracha', 35, [3,4]),
@@ -28,7 +62,7 @@ function fetchElectionData() {
     fetch("http://www.mocky.io/v2/5a55224b2d000088425b1ed8")
         .then(response => response.json())
         .then(data =>addVoters())
-        .then(data =>candidates())
+        .then(data =>createCandidates())
         .then(filterInvalidVoters() )
         .then(runElection())
         .then(getWinner())
@@ -38,82 +72,18 @@ function fetchElectionData() {
 
 function addVoters(data){
     data.voters.forEach(person => {
+        console.log(person);
         votingPopulation.push(new Voter(person.name, person.age, person.votingCard));
     })
 }
         
 
 
-function candidates(data){
+function createCandidates(data){
     data.candidates.forEach(candidates => {
         candidates.push(new Candidate(candidates.name, candidates.age, candidates.votingCard,candidates.party));
     })
 }        
-       
-
-        fetchElectionData();
-
-
-
-/**
- * 1 - Write a Voter class modelling a member of the population 
- * who votes in the election.
- */
-class Voter {
-    constructor(name,age,votingCard){
-        this.name = name;
-        this.age = age;
-        this.votingCard = votingCard
-    }
-}
-
-
-
-/**
- * 2 - Write a Candidate class modelling a candidate 
- * in the election. Candidates are also voters (they 
- * can vote for themselves, or anyone else).
- * However they have some extra properties.
- */
-
-class Candidate extends Voter {
-	constructor(name, age, votingCard, party) {
-		super(name,age,votingCard);
-        this.party = party;
-        this.numVotes= 0;
-	}
-};
-
-
-/**
- * 3 - Write an Election class which models the election.
- */
-class Election {
-    constructor(validVoters,candidates) {
-        this.validVoters = validVoters;
-        this.candidates = candidates;
-        this.winner = " ";
-    }
-
-    runElection() {
-        this.candidates = runElection(this.validVoters, this.candidates);
-          this.getWinner();
-    }
-    
-    
-    
-    getWinner() {
-        this.winner = getWinner(this.candidates);
-    };
-
-    printWinnerMessage() {
-        return  winnerMessage(this.winner);
-    }
-}
-
-
-
-
 let allVoters = votingPopulation.concat(candidatesObjToArray(candidates));
 
 let validVoters = filterInvalidVoters(allVoters);
